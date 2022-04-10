ca449283932d102c1aa0a803cc462ba8c4fca08f
Draglesss <dangerpop-otmane@hotmail.com>
2022-04-10 23:05:34 +0000

    improvements

--- TicTacToe.py
+++ TicTacToe.py
@@ -27,23 +27,29 @@ class board :
 
     def check_win(self) :
         if self.board[1] == self.board[2] == self.board[3] :
-            return [True, self.board[1]]
+            return True, self.board[1]
         elif self.board[4] == self.board[5] == self.board[6] :
-            return [True, self.board[4]]
+            return True, self.board[4]
         elif self.board[7] == self.board[8] == self.board[9] :
-            return [True, self.board[7]]
+            return True, self.board[7]
         elif self.board[1] == self.board[4] == self.board[7] :
-            return [True, self.board[1]]
+            return True, self.board[1]
         elif self.board[2] == self.board[5] == self.board[8] :
-            return [True, self.board[2]]
+            return True, self.board[2]
         elif self.board[3] == self.board[6] == self.board[9] :
-            return [True, self.board[3]]
+            return True, self.board[3]
         elif self.board[1] == self.board[5] == self.board[9] :
-            return [True, self.board[1]]
+            return True, self.board[1]
         elif self.board[3] == self.board[5] == self.board[7] :
-            return [True, self.board[3]]
+            return True, self.board[3]
         else :
-            return [False, False]
+            return False, False
+
+    def check_tie(self) :
+        return self.isfull()
+           
+    def new_board(self) :
+        self.board = ['', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
 my_board = board()
 my_board.print_board()
@@ -74,15 +80,15 @@ def run(board) :
         if board.check_win()[0] :
             print(board.check_win()[1], ' wins')
             break
-    print('Game Over')
+    if board.check_tie() and board.check_win()[0] == False :
+        print('Tie')
 replay = True
 run(my_board)
 while replay :
     choice = input('Do you want to play again? (y/n) ')
     if choice == 'y' :
         clearConsole()
-        del my_board
-        my_board = board()
+        my_board.new_board()
         my_board.print_board()
         run(my_board)
     elif choice == 'n' :
@@ -93,3 +99,4 @@ while replay :
         print('Invalid input')
         time.sleep(2)
         clearConsole()
+        my_board.print_board()
